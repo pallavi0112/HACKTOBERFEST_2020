2da75c724887040bb67baf56282f9cbe8010382b
Pritindra <37242669+pritindra@users.noreply.github.com>
2020-10-27 00:52:17 +0530

    Add files via upload

---
+++ Golang/insertionSort.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
+
+func main() {
+
+	slice := generateSlice(20)
+	fmt.Println("\n--- Unsorted --- \n\n", slice)
+	insertionSort(slice)
+	fmt.Println("\n--- Sorted ---\n\n", slice, "\n")
+}
+
+func generateSlice(size int) []int {
+
+	slice := make([]int, size, size)
+	rand.Seed(time.Now().UnixNano())
+	for i := 0; i < size; i++ {
+		slice[i] = rand.Intn(999) - rand.Intn(999)
+	}
+	return slice
+}
+
+func insertionSort(arr []int) {
+	var size = len(arr)
+	for i := 1; i < size; i++ {
+		var key = arr[i]
+		var j int = i - 1
+
+		for j >= 0 && arr[j] > key {
+			arr[j+1] = arr[j]
+			j--
+		}
+		arr[j+1] = key
+
+	}
+
+}
